9409f9f244b45419a7326bf494f6fa06d2803e17
Kevin F. CALMO <kevinfcalmo@gmail.com>
2025-02-10 19:41:59 -0400

    Ajout d'un premier client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod commerce;
+mod utils;
+
+use commerce::GestionCommerciale;
+
 fn main() {
-    println!("Hello, world!");
+    let mut gestion = GestionCommerciale::new();
+    gestion.ajouter_client(
+        "Alice".to_string(),
+        "alice@example.com".to_string(),
+        "123 Rue Principale".to_string(),
+        100.0,
+    );
 }
